a8a677de9147e8aeef757f65d24f00d94379ea1b
Hartono Chandra <harchcode@gmail.com>
2022-11-19 08:18:37 +0700

    fix build script and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,38 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-export default defineConfig({
-  test: {
-    environment: "jsdom",
-    watch: false,
-    globals: true,
-    coverage: {
-      include: ["src/**/*.ts"],
-      all: true,
-      statements: 0,
-      branches: 0,
-      functions: 0,
-      lines: 0
-    }
-  },
-  build: {
-    target: "ESNext",
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "Web Digraph",
-      // the proper extensions will be added
-      fileName: "web-digraph"
-    }
+export default defineConfig(() => {
+  if (process.env.BUILD_TYPE === "example") {
+    return {
+      build: {
+        outDir: "dist-example"
+      }
+    };
+  } else {
+    return {
+      test: {
+        environment: "jsdom",
+        watch: false,
+        globals: true,
+        coverage: {
+          include: ["src/**/*.ts"],
+          all: true,
+          statements: 0,
+          branches: 0,
+          functions: 0,
+          lines: 0
+        }
+      },
+      build: {
+        target: "ESNext",
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, "src/index.ts"),
+          name: "Web Digraph",
+          // the proper extensions will be added
+          fileName: "web-digraph"
+        }
+      }
+    };
   }
 });
